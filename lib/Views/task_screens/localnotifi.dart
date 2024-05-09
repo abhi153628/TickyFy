879007b1ef04fe99f9dc12e426f78e9a8e0a25a2
Abhishek R <abhishekramesh2424@gmail.com>
2024-05-09 14:57:18 +0530

    added flutter local notification

--- lib/Views/task_screens/localnotifi.dart
+++ lib/Views/task_screens/localnotifi.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotificationService {
   static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
   static onTap(NotificationResponse) {}
+
   static Future init() async {
-    InitializationSettings settings = InitializationSettings(
+    InitializationSettings settings = const InitializationSettings(
         android: AndroidInitializationSettings('@mipmap/ic_launcher'),
         iOS: DarwinInitializationSettings());
     flutterLocalNotificationsPlugin.initialize(settings,
@@ -13,43 +18,34 @@ class LocalNotificationService {
         onDidReceiveNotificationResponse: onTap);
   }
 
-  //1.basic notification
-  static showBasicNotification() async {
-    NotificationDetails details = NotificationDetails(
-        android: AndroidNotificationDetails('id 1', 'basic notification',
-            importance: Importance.max, priority: Priority.high));
-    await flutterLocalNotificationsPlugin.show(
-        0, 'Basic notification', 'body', details,
-        payload: 'playload Data');
+  static Future<void> showDailyScheduledNotification({TimeOfDay? time, required String question}) async {
+    if (time!= null) {
+      tz.initializeTimeZones();
+      tz.setLocalLocation(tz.getLocation('Asia/Kolkata')); // Set your desired time zone
+      final now = DateTime.now();
+      final scheduledDateTime = DateTime(now.year, now.month, now.day, time.hour, time.minute);
+
+      await flutterLocalNotificationsPlugin.zonedSchedule(
+        3,
+        'TickyFy',
+        'Did you do your habit',
+        tz.TZDateTime.from(scheduledDateTime, tz.local),
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'id4',
+            'Daily Scheduled Notification',
+            importance: Importance.max,
+            priority: Priority.high,
+          ),
+        ),
+        uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time,
+        payload: 'daily_notification',
+      );
+    }
   }
 
-
-  //Repeated Notification
-    static showRepeatedNotification() async {
-    NotificationDetails details = NotificationDetails(
-        android: AndroidNotificationDetails('id 2', 'repeated notification',
-            importance: Importance.max, priority: Priority.high));
-    await flutterLocalNotificationsPlugin.periodicallyShow(
-        1, 'Repeated notification', 'body',RepeatInterval.everyMinute, details,
-        payload: 'playload Data');
-  }
-
-
-  //shedule notification
-
-   static showSheduleNotification() async {
-    NotificationDetails details = NotificationDetails(
-        android: AndroidNotificationDetails('id 2', 'shedule notification',
-            importance: Importance.max, priority: Priority.high));
-    await flutterLocalNotificationsPlugin.periodicallyShow(
-        1, 'Scheduled notification', 'body',RepeatInterval.everyMinute, details,
-        payload: 'playload Data');
-  }
- static void cancelNotification(int id )async{
+  static Future<void> cancelNotification(int id) async {
     await flutterLocalNotificationsPlugin.cancel(id);
   }
 }
-//1.setup
-//2.basic notification
-//repeated notification
-//4.sheduled notofication
